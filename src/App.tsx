9d0fbee58a361f6f2e0620d6145507f2783f54e1
Avan <layouwen@gmail.com>
2024-06-04 11:01:52 +0800

    chore: init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {Tldraw} from 'tldraw'
+import './index.css'
+
+function App() {
+    return (
+        <div style={{position: 'fixed', inset: 0}}>
+            <Tldraw/>
+        </div>
+    )
+}
+
+export default App
